162f8cb8c587c25c1b3ebee0a9cecfbbde84473f
CalcTrice <mycalctrice@gmail.com>
2021-03-22 17:33:09 +0530

    Locomotive Scroll CDN Added

--- JS/libraryVendors.js
+++ JS/libraryVendors.js
@@ -1,6 +1,6 @@
 
 //Locomotive Scroll
-import LocomotiveScroll from 'locomotive-scroll';
+// import LocomotiveScroll from 'locomotive-scroll';
 
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
